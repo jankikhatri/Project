a10bf294db05aaf1ebaa4996ade50952deb86228
jankikhatri <jankikhatri72@gmail.com>
2020-05-02 18:03:14 -0400

    Project

---
+++ js/home.js
@@ -0,0 +1,17 @@
+let slideIndex = 0;
+carousel();
+
+function  carousel() {
+    let i;
+    let slides = document.getElementsByClassName("item")
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
